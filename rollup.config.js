961df5f039dab68e8650eda87ee37c1b9a4ea7d3
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-03-16 15:07:48 +0100

    feat: add babel, eslint and rollup config

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import { terser } from "rollup-plugin-terser";
+
+import pkg from './package.json';
+
+export default {
+	input: 'src/index.js',
+	output: [
+		{ file: pkg.main, format: 'cjs' },
+		{ file: pkg.module, format: 'es' },
+	],
+	plugins: [
+		resolve({
+			extensions: ['.js', '.jsx'],
+		}),
+		babel(),
+		terser(),
+	],
+	external: [
+		'prop-types',
+		'react',
+	],
+};
