fb480065a54f95c28838b0892623aea86b98c0fc
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-03-17 14:48:11 +0100

    feat(table): add basic table

---
+++ src/components/Table/Table.helpers.js
@@ -0,0 +1,46 @@
+const getCellValue = (rowData, key) => String(rowData[key]) || '';
+
+const getFormatValue = (rowData, col, rowIndex) => {
+	if (typeof col === 'string') {
+		return getCellValue(rowData, col);
+	}
+
+	const cellValue = getCellValue(rowData, col.value);
+
+	return col.format
+		? col.format(cellValue, col, rowData, rowIndex)
+		: cellValue;
+};
+
+export const getHeaderProps = (col, activeSorting, onSortClick) => {
+	const keyPrefix = 'table-header';
+
+	if (typeof col === 'string') {
+		return { key: `${keyPrefix}-${col}`, label: col }
+	}
+
+	return {
+		key: `${keyPrefix}-${col.label}`,
+		component: col.headerComponent,
+		label: col.label,
+		value: col.value,
+		activeSorting,
+		onSortClick,
+	}
+};
+
+export const getCellProps = (col, rowData, rowIndex) => {
+	const keyPrefix = 'table-cell';
+
+	if (typeof col === 'string') {
+		return { key: `${keyPrefix}-${col}`, label: col }
+	}
+
+	return {
+		key: `${keyPrefix}-${col.label}`,
+		component: col.component,
+		rowData,
+		rowIndex,
+		value: getFormatValue(rowData, col, rowIndex),
+	}
+};
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,99 @@
+import classnames from 'classnames';
+import React from 'react';
+
+import { DEFAULT_MESSAGE, tablePropTypes } from './Table.const';
+import { getHeaderProps, getCellProps } from './Table.helpers';
+import TableCell from './TableCell/TableCell';
+import TableHeader from './TableHeader/TableHeader';
+import TablePlaceholder from './TablePlaceholder/TablePlaceholder';
+import TableRow from './TableRow/TableRow';
+
+const Table = ({
+	className,
+	rows = [],
+	columns = [],
+	loading = false,
+	responsive = true,
+	hasClickAction = false,
+	activeSorting,
+	orderBy,
+	rowClicked,
+	striped = true,
+	type,
+	loadDataMessage = DEFAULT_MESSAGE.loading,
+	noColumnsMessage = DEFAULT_MESSAGE.noColumns,
+	noDataMesssage = DEFAULT_MESSAGE.noData,
+}) => {
+	// Computed
+	const hasCols = !loading && columns.length > 0;
+	const hasData = !loading && rows.length > 0;
+	const showPlaceholder = loading || !hasCols || !hasData;
+
+	/**
+	 * Methods
+	 */
+	const onRowClick = (rowData) => {
+		if (hasClickAction && rowClicked) {
+			rowClicked(rowData);
+		}
+	};
+
+	const onSortClick = (key, order) => {
+		if (orderBy) {
+			orderBy({ key, order });
+		}
+	};
+
+	/**
+	 * Render
+	 */
+	return (
+		<div className={classnames(className, { 'a-table__wrapper-responsive': responsive })}>
+			<table
+				className={classnames('a-table', {
+					'a-table--striped': striped,
+					[`a-table--${type}`]: type,
+				})}
+			>
+				{columns.length > 0 && (
+					<thead>
+						<TableRow>
+							{columns.map(col => (
+								<TableHeader {...getHeaderProps(col, activeSorting, onSortClick)} />
+							))}
+						</TableRow>
+					</thead>
+				)}
+				<tbody>
+					{showPlaceholder ? (
+						<TablePlaceholder
+							colSpan={columns.length}
+							hasCols={hasCols}
+							hasData={hasData}
+							loading={loading}
+							noDataMesssage={noDataMesssage}
+							loadDataMessage={loadDataMessage}
+							noColumnsMessage={noColumnsMessage}
+						/>
+					) : (
+						rows.map((row, rowIndex) => (
+							<TableRow
+								key={`table-row-${rowIndex}`}
+								hasClickAction={hasClickAction}
+								onClick={() => onRowClick(row)}
+							>
+								{columns.map(col => (
+									<TableCell {...getCellProps(col, row, rowIndex)} />
+								))}
+							</TableRow>
+						))
+					)}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+Table.propTypes = {...tablePropTypes};
+
+export default Table;
