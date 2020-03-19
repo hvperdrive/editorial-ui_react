a65441a17cf75beb1d042e8e6c6a96df3099d460
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2020-03-19 10:59:58 +0100

    fix: use direct styles

--- src/components/ActionBar/ActionBar.jsx
+++ src/components/ActionBar/ActionBar.jsx
@@ -1,13 +1,9 @@
-import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import { useSlot } from '../../hooks/useSlot';
 
 import styles from './ActionBar.module.scss';
-
-const cx = classNames.bind(styles);
-
 
 export const ActionBarContentSection = ({ children }) => <>{children}</>;
 
@@ -19,7 +15,7 @@ const ActionBar = ({ children }) => {
 	const contentSlot = useSlot(ActionBarContentSection, children);
 
 	return (
-		<div className={cx('o-action-bar')}>
+		<div className={styles['o-action-bar']}>
 			<div className="u-container">
 				<div className="o-action-bar__content u-margin-top-xs u-margin-bottom-xs">
 					{contentSlot}
