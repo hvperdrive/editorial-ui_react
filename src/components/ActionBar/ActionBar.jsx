12a532b8c1402a43eb6b6b77b057c3c1b0efe8d8
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2020-03-19 17:50:54 +0100

    fix: bind classnames

--- src/components/ActionBar/ActionBar.jsx
+++ src/components/ActionBar/ActionBar.jsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -7,6 +8,8 @@ import styles from './ActionBar.module.scss';
 
 export const ActionBarContentSection = ({ children }) => <>{children}</>;
 
+const cx = classNames.bind(styles);
+
 ActionBarContentSection.propTypes = {
 	children: PropTypes.node,
 };
@@ -15,7 +18,7 @@ const ActionBar = ({ children }) => {
 	const contentSlot = useSlot(ActionBarContentSection, children);
 
 	return (
-		<div className={styles['o-action-bar']}>
+		<div className={cx('o-action-bar')}>
 			<div className="u-container">
 				<div className="o-action-bar__content u-margin-top-xs u-margin-bottom-xs">
 					{contentSlot}
