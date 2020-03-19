f13fa9d1c76d570261138695b017188755804056
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-03-19 16:48:41 +0100

    feat: add paginated table

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Example } from './Example/Example';
+export { default as PaginatedTable } from './PaginatedTable/PaginatedTable';
 export { default as Table } from './Table/Table';
