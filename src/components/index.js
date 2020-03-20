dbbf61a3aeb70596f4141a8a702596726ab63fb0
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-03-20 14:22:41 +0100

    fix(contextHeader): export slot components

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as PaginatedTable } from './PaginatedTable/PaginatedTable';
 export { default as Table } from './Table/Table';
 export { default as ActionBar, ActionBarContentSection } from './ActionBar/ActionBar';
 export { default as ContextHeader } from './ContextHeader/ContextHeader';
+export { ContextHeaderActionsSection, ContextHeaderTopSection } from './ContextHeader/ContextHeader.slots';
