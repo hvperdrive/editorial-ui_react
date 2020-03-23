cc83a600f16bad8881a5f6a725abc1391278a415
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-03-23 10:57:25 +0100

    fix(storybook): code styling

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import { addDecorator } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 
+import './storybook.scss';
+
 // Configure global addons
 addDecorator(withA11y);
