4ee78eebf12e4e6599332de6cdc1e5589ab24b95
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-03-23 09:34:09 +0100

    feat(contextHeader): export slot hook and components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Example } from './Example/Example';
 export { default as Table } from './Table/Table';
 export { default as ContextHeader } from './ContextHeader/ContextHeader';
+export { ContextHeaderActionsSection, ContextHeaderTopSection } from './ContextHeader/ContextHeader.slots';
