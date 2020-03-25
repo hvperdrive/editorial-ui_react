3db7ed77893b16ef83991a6ed3de9533c2f4d05b
maximdegeyter <maximdegeyter@outlook.com>
2020-03-25 09:03:52 +0100

    feat: add filter stories

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,39 +1,67 @@
 // eslint-disable-next-line max-len
 import {
-	Accordion, AccordionTab, AccordionTabContent, AccordionTabHeader, Button, TextField,
+	Accordion, AccordionTab, AccordionTabContent, AccordionTabHeader, Button, TagList, TagListItem,
 } from '@acpaas-ui/react-components';
 // import classnames from 'classnames';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const Filter = () => {
-	return (
-		<div>
-			<div className="u-container">
-				<Accordion>
-					<AccordionTab>
-						<AccordionTabHeader>Filter</AccordionTabHeader>
-						<AccordionTabContent>
-							<TextField
-								label="Zoeken"
-								id="textfield-id-3"
-								name="textfield-name"
-								className="textfield-class"
-								placeholder="Zoeken op naam, beschrijving..."
-								onChange={(value) => console.log('Filteren op:', value)}
-								iconright="search"
+const Filter = ({
+	title,
+	filters,
+	onConfirm,
+	onClean,
+	activeFilters = [],
+	onFilterRemove,
+}) => (
+	<div>
+		<div className="u-container">
+			<Accordion>
+				<AccordionTab>
+					<AccordionTabHeader><h3 className="u-text-primary">{title}</h3></AccordionTabHeader>
+					<AccordionTabContent>
+						{filters ? (
+							<div>{ filters }</div>
+						) : (
+							<p>Geen filters beschikbaar</p>
+						)}
+						<div className="u-margin-top u-text-right">
+							<Button type="primary" onClick={onClean} negative>Alles leegmaken</Button>
+							<Button type="primary" iconRight="angle-right" onClick={onConfirm} outline>Toepassen</Button>
+						</div>
+					</AccordionTabContent>
+				</AccordionTab>
+			</Accordion>
+			{ activeFilters.length > 0 ? (
+				<div className="u-margin-top">
+					<TagList>
+						{activeFilters.map((filter) => (
+							<TagListItem
+								value={filter.value}
+								key={filter.key}
+								closable
+								onClick={onFilterRemove}
 							/>
-							<Button type="primary" negative>Alles leegmaken</Button>
-							<Button type="primary" iconright="chevron-right" outline>Toepassen</Button>
-						</AccordionTabContent>
-					</AccordionTab>
-				</Accordion>
-			</div>
+						))}
+					</TagList>
+				</div>
+			) : (
+				''
+			)}
 		</div>
-	);
-};
+	</div>
+);
 
 Filter.propTypes = {
+	title: PropTypes.string,
+	onConfirm: PropTypes.func,
+	onClean: PropTypes.func,
+	activeFilters: PropTypes.arrayOf(PropTypes.shape({
+		label: PropTypes.string,
+		value: PropTypes.string,
+	})),
+	onFilterRemove: PropTypes.func,
+	filters: PropTypes.any,// eslint-disable-line
 };
 
 export default Filter;
