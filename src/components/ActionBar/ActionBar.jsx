5620f5035a51a34e85cbbc8c1344d9f1c370309d
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-03-27 15:57:02 +0100

    feat: render ActionBar through portal

--- src/components/ActionBar/ActionBar.jsx
+++ src/components/ActionBar/ActionBar.jsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 
 import { useSlot } from '../../hooks';
 
@@ -9,11 +10,18 @@ import { ActionBarContentSection } from './ActionBar.slots';
 
 const cx = classNames.bind(styles);
 
-
-const ActionBar = ({ children }) => {
+const ActionBar = ({ children, container, disablePortal = false }) => {
+	const [mountNode, setMountNode] = useState(null);
 	const contentSlot = useSlot(ActionBarContentSection, children);
 
-	return (
+	useEffect(() => {
+		if (!disablePortal) {
+			const node = container || document.body;
+			setMountNode(node);
+		}
+	}, [container, disablePortal]);
+
+	const renderActionBar = () => (
 		<div className={cx('o-action-bar')}>
 			<div className="u-container">
 				<div className="o-action-bar__content u-margin-top-xs u-margin-bottom-xs">
@@ -22,10 +30,16 @@ const ActionBar = ({ children }) => {
 			</div>
 		</div>
 	);
+
+	return !disablePortal && mountNode
+		? createPortal(renderActionBar(), mountNode)
+		: renderActionBar();
 };
 
 ActionBar.propTypes = {
 	children: PropTypes.node,
+	container: PropTypes.node,
+	disablePortal: PropTypes.bool,
 };
 
 export default ActionBar;
