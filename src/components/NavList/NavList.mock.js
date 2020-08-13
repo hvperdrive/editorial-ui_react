b0393c583640505983fca97ffd6687f975785b72
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-08-13 11:48:27 +0200

    docs(NavList): add stories

---
+++ src/components/NavList/NavList.mock.js
@@ -0,0 +1,17 @@
+export const MOCK_ITEMS = [
+	{ href: '#link-1', title: 'Link 1', label: 'Link 1' },
+	{ href: '#link-2', title: 'Link 2', label: 'Link 2' },
+	{ href: '#link-3', title: 'Link 3', label: 'Link 3' },
+];
+
+export const MOCK_ITEMS_ERROR = [
+	{
+		href: '#link-1', title: 'Link 1', label: 'Link 1', hasError: false,
+	},
+	{
+		href: '#link-2', title: 'Link 2', label: 'Link 2', hasError: true,
+	},
+	{
+		href: '#link-3', title: 'Link 3', label: 'Link 3', hasError: false,
+	},
+];
