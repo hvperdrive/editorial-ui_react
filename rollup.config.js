d976546af8c3b4b869de2d1eb7726418d1f525c5
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-11-09 14:22:32 +0100

    chore: fix build add commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import autoprefixer from 'autoprefixer';
+import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
@@ -13,6 +14,9 @@ export default {
 		{ file: pkg.module, format: 'es' },
 	],
 	plugins: [
+		commonjs({
+			include: 'node_modules/**',
+		}),
 		resolve({
 			extensions: ['.js', '.jsx', '.scss'],
 		}),
