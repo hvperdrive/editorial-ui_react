2ed216c5fb8e80ae247480b1906336c6e222d1ba
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2021-02-02 14:01:32 +0100

    feat: add tooltip and ellipsisWithTooltip component

--- src/components/Table/TableCell/TableCell.jsx
+++ src/components/Table/TableCell/TableCell.jsx
@@ -2,20 +2,32 @@ import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { EllipsisWithTooltip } from '../../EllipsisWithTooltip';
+
 const TableCell = ({
 	classList,
 	component,
 	rowData,
 	rowIndex,
 	value,
+	ellipsis = false,
 }) => (
 	<td className={classnames(classList)}>
-		{component ? component(value, rowData, rowIndex) : value}
+		{ ellipsis ? (
+			<EllipsisWithTooltip type="primary">
+				{component ? component(value, rowData, rowIndex) : value}
+			</EllipsisWithTooltip>
+		) : (
+			<>
+				{component ? component(value, rowData, rowIndex) : value}
+			</>
+		)}
 	</td>
 );
 
 TableCell.propTypes = {
 	classList: PropTypes.arrayOf(PropTypes.string),
+	ellipsis: PropTypes.bool,
 	component: PropTypes.func,
 	rowData: PropTypes.oneOfType([PropTypes.object]),
 	rowIndex: PropTypes.number,
