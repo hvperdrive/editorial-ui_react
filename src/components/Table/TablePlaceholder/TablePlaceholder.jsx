69ce7c4b6d2a63d6060798899f5611b951cbe4a0
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2021-02-15 15:27:30 +0100

    chore(Table): remove unnecessary comments

--- src/components/Table/TablePlaceholder/TablePlaceholder.jsx
+++ src/components/Table/TablePlaceholder/TablePlaceholder.jsx
@@ -21,11 +21,8 @@ const TablePlaceholder = ({
 };
 
 TablePlaceholder.propTypes = {
-	// colSpan: PropTypes.number,
 	hasCols: PropTypes.bool.isRequired,
 	hasData: PropTypes.bool.isRequired,
-	// loading: PropTypes.bool.isRequired,
-	// loadDataMessage: PropTypes.string,
 	noColumnsMessage: PropTypes.string,
 	noDataMessage: PropTypes.string,
 };
