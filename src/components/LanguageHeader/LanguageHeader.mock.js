5c160062827a457af5fe250e2d28320bd920bbb6
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2022-03-03 11:50:20 +0100

    feat(red-1984): map values based on selected language

--- src/components/LanguageHeader/LanguageHeader.mock.js
+++ src/components/LanguageHeader/LanguageHeader.mock.js
@@ -1,3 +1,13 @@
 export const LANGUAGE_HEADER_MOCK_LANGUAGES = ['NL', 'FR', 'DE', 'EN'];
 
+// TODO: map languages to object
+/* [
+	{
+		key: 'nl',
+		primary: true,
+	},
+]; */
+
 export const LANGUAGE_HEADER_MOCK_TOOLTIP = 'Er zijn opties voor meerder talen in dit formulier. Selecteer een taal om specifieke instellingen te configureren.';
+
+export const INITIAL_VALUES_MOCK = { title: '', description: { nl: 'nederlands', fr: 'frans', multilanguage: true } };
--- src/components/MultilanguageField/MultilanguageField.jsx
+++ src/components/MultilanguageField/MultilanguageField.jsx
@@ -1,17 +1,56 @@
 import { Icon } from '@acpaas-ui/react-components';
 import classNames from 'classnames/bind';
 import { Field } from 'formik';
+import PropTypes from 'prop-types';
+import { pathOr } from 'ramda';
 import React from 'react';
 
 import styles from './MultilanguageField.module.scss';
 
 const cx = classNames.bind(styles);
 
-const MultilanguageField = (props) => (
-	<div className={cx('u-bg-light', 'o-multilanguage-field')}>
-		<Icon name="globe" className={cx('o-multilanguage-field__icon')} />
-		<Field {...props} />
-	</div>
-);
+const MultilanguageField = ({
+	activeLanguage, languages, setFieldValue, ...props
+}) => {
+	const activeLang = activeLanguage.toLowerCase();
+
+	const getFieldValue = () => {
+		// TODO: remove value if languages does not contain key of value
+
+		// if multilanguage, get value for active language
+		if (pathOr(false, ['multilanguage'], props.value)) {
+			const val = pathOr('', [activeLang], props.value);
+
+			return val;
+		}
+
+		// else, create multilanguage object and set empty value for active language
+		setFieldValue(props.name, {
+			multilanguage: true,
+			[activeLang]: '',
+		});
+
+		return '';
+	};
+	return (
+		<div className={cx('u-bg-light', 'o-multilanguage-field')}>
+			<Icon name="globe" className={cx('o-multilanguage-field__icon')} />
+			<Field
+				{...props}
+				name={`${props.name}.${activeLang}`}
+				value={getFieldValue()}
+			/>
+		</div>
+	);
+};
+
+MultilanguageField.propTypes = {
+	activeLanguage: PropTypes.string.isRequired,
+	languages: PropTypes.arrayOf(PropTypes.string).isRequired,
+	name: PropTypes.string.isRequired,
+	// eslint-disable-next-line react/forbid-prop-types
+	value: PropTypes.any,
+	setFieldValue: PropTypes.func,
+};
 
 export default MultilanguageField;
