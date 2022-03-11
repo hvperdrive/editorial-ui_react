7b8c56035fccbf0c4435aad1b570e701adc536ee
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2022-03-11 10:53:20 +0100

    feat(red-1984): useMemo

--- src/components/MultilanguageField/MultilanguageField.jsx
+++ src/components/MultilanguageField/MultilanguageField.jsx
@@ -1,16 +1,14 @@
 import { Icon } from '@acpaas-ui/react-components';
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import styles from './MultilanguageField.module.scss';
 
 const cx = classNames.bind(styles);
 
 const MultilanguageField = ({ asComponent, ...props }) => {
-	console.log({ asComponent, ...props });
-
-	const FormField = () => React.createElement(asComponent, props);
+	const FormField = useMemo(() => React.createElement(asComponent, props), [asComponent, props]);
 
 	return (
 		<div className={cx('u-bg-light', 'o-multilanguage-field')}>
